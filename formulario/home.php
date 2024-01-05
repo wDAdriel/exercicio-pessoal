caf7298f44ad1f08018e6c266bea6bc629fb08e1
AdrielSantana <144896671+wDAdriel@users.noreply.github.com>
2024-01-05 10:08:05 -0300

    css formulario

--- formulario/home.php
+++ formulario/home.php
@@ -8,7 +8,8 @@
 <body>
     <h1>Confirmação</h1>  
     <?php
-    
+        $email = $_POST["email"];
+        echo "<p>Para concluir seu cadastro confirme o e-mail enviado para $email </p>";
     ?>php
 </body>
 </html>
